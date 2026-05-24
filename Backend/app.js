aba43020d1d0a0960a82c9fcbb1595e1d9bbe9c4
Adriana <adrianamontoyaa9@gmail.com>
2026-05-24 16:21:59 -0600

    feat: Se crearon los controllers de loginCustomer, recoveryPassword y las rutas de cada uno

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import CustomerRoutes from "../Backend/src/routes/Customer.js";
 import LogoutRoutes from "../Backend/src/routes/Logout.js";
+import loginCustomerRoutes from "../Backend/src/routes/loginCustomers.js";
+import recoveryPasswordRoutes from "../Backend/src/routes/recoveryPassword.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -19,6 +21,8 @@ app.use(express.json());
 
 app.use("/api/customers", CustomerRoutes);
 app.use("/api/logout", LogoutRoutes);
+app.use("/api/login",loginCustomerRoutes);
+app.use("/api/recoveryPassword", recoveryPasswordRoutes);
 
 
 export default app;
